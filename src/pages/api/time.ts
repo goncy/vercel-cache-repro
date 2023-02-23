d89a5cee4a5d8901a0af2c9fcdc95378bb8e75ea
goncy <gonzalo.pozzo4@gmail.com>
2023-02-23 20:55:08 -0300

    Use vercel url

--- src/pages/api/time.ts
+++ src/pages/api/time.ts
@@ -1,5 +1,5 @@
-import {NextApiRequest, NextApiResponse} from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default function time(_req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).send(new Date().toISOString());
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+  return res.send(new Date().toISOString())
 }
