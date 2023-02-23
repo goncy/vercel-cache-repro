e2cce0236e6657e386134628847ba365d0f454e8
goncy <gonzalo.pozzo4@gmail.com>
2023-02-23 20:37:55 -0300

    Move api to route handler

--- src/pages/api/time.ts
+++
@@ -1,5 +0,0 @@
-import {NextApiRequest, NextApiResponse} from "next";
-
-export default function time(_req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).send(new Date().toISOString());
-}
