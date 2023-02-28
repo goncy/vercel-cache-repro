06cc3990003a1a30d67e7622154a4390eb8970da
goncy <gonzalo.pozzo4@gmail.com>
2023-02-28 10:06:54 -0300

    Remove params

--- src/app/hello/route.ts
+++ src/app/hello/route.ts
@@ -1,16 +1,14 @@
-import type { NextRequest } from "next/server";
-
 export const dynamic = 'force-static'
 export const revalidate = 10
 
-export function GET(request: NextRequest) {
+export function GET() {
   return new Response(`
   <html>
     <head>
       <title>Hello</title>
     </head>
     <body>
-      <h1>Hello ${request.nextUrl.searchParams.get('name')}</h1>
+      <h1>Hello</h1>
       <p>This will be revalidated every 10 seconds!</p>
       <p>Last revalidation: ${new Date()}</p>
     </body>
